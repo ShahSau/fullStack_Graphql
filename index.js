882152c656f9305c0ab44c5e7a681e1ef40cf8c7
shahriar <shahriarksaurov@gmail.com>
2024-12-31 16:11:46 +0100

    8.2: All books

--- index.js
+++ index.js
@@ -82,16 +82,26 @@ let books = [
 
 
 const typeDefs = `
-  type Query {
-    bookCount: Int!
-    authorCount: Int!
-  }
+    type Book{
+        title: String!
+        author: String!
+        published: Int!
+        genres: [String!]!
+    }
+
+
+    type Query {
+        bookCount: Int!
+        authorCount: Int!
+        allBooks: [Book!]!
+    }
 `
 
 const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
+    allBooks: () => books
   }
 }
 
